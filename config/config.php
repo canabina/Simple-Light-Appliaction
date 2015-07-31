1decb9f5bca2bf37272ba0aa1b86714e93f8a82a
altuhov <konstantin255@hmail.com>
2015-07-31 14:08:22 +0300

    Add empty floders

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+		'db' => [
+			'host' => '',
+			'login' => '',
+			'password' => '',
+			'database' => '',
+			'optional' => [
+						'charset' => 'utf8',
+						//sqlite, mysql, pgsql
+						'database_type' => 'mysql'
+					],
+			],
+		'forntend_module_folder' => 'frontend',
+		'siteName' => 'Web App',
+		'compiledDir' => 'runtime/compiled'
+
+];
